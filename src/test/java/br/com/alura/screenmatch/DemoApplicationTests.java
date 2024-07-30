0245cbcdb53422151290aacfd009bcf66b49a0c9
Mateus Pontes <mateuspsdd@gmail.com>
2024-07-30 09:56:32 -0300

    ...

--- src/test/java/br/com/alura/screenmatch/DemoApplicationTests.java
+++ src/test/java/br/com/alura/screenmatch/DemoApplicationTests.java
@@ -1,12 +1,11 @@
 package br.com.alura.screenmatch;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 }
